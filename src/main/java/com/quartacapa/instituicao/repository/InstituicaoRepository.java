85fff8b131221650eeea40ac579193b2c96d752d
hennangadelha <hennanhennan@gmail.com>
2021-09-21 20:18:53 -0300

    filtrando instituicao por nome

--- src/main/java/com/quartacapa/instituicao/repository/InstituicaoRepository.java
+++ src/main/java/com/quartacapa/instituicao/repository/InstituicaoRepository.java
@@ -3,5 +3,10 @@ package com.quartacapa.instituicao.repository;
 import com.quartacapa.instituicao.model.Instituicao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InstituicaoRepository extends JpaRepository<Instituicao, String> {
+
+    Optional<Instituicao> findByNome(String nome);
+
 }
