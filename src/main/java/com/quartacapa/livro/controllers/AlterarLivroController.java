be4d1544722618a4dd13925c8f4362b8b58a3bba
hennangadelha <hennanhennan@gmail.com>
2021-09-27 18:45:24 -0300

    corrigindo bug para alterar disciplina do livro

--- src/main/java/com/quartacapa/livro/controllers/AlterarLivroController.java
+++ src/main/java/com/quartacapa/livro/controllers/AlterarLivroController.java
@@ -70,6 +70,7 @@ public class AlterarLivroController {
         livroAlterado.setDescricaoEstado(request.getDescricaoEstado());
         livroAlterado.setDisponivelParaDoacao(request.getDisponivelParaDoacao());
 
+        //busca disciplina
         Optional<Disciplina> possivelDisciplina = disciplinaRepository.findById(request.getIdDisciplina());
 
         Disciplina disciplina = possivelDisciplina.get();
